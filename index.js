9cb55d90f7b4ab5142ea8dcfc6bd1195118a62ab
Roman Karki <code.karki.roman@gmail.com>
2023-11-30 13:02:03 +0545

    problem 2 and 3

--- index.js
+++ index.js
@@ -13,3 +13,34 @@ asyncFunction().then(res=>{
 })
 
 // Output: Finally block executed
+
+function problemTwo(){
+    let a= 2;
+    {
+        let a=4
+    }
+    console.log(a+1)
+}
+
+problemTwo()
+
+//output : 3
+
+
+
+//Problem 3 
+
+const createAsyncFunction = (ms,result)=> async ()=>{
+    await new Promise(resolve=> setTimeout(resolve,ms))
+    return result
+}
+
+const asyncFunctions = [
+    createAsyncFunction(1000,"A"),
+    createAsyncFunction(500,"B"),
+    createAsyncFunction(2000,"C")
+]
+
+Promise.race(asyncFunctions.map(fn=>fn())).then(res=>console.log(res))
+
+// Output : B
