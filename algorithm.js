56cd1ab3aaf4e82ccff0327131f11f5cf32c933a
Roman Karki <code.karki.roman@gmail.com>
2023-12-01 13:38:56 +0545

    polyfill for the map function

--- algorithm.js
+++ algorithm.js
@@ -17,3 +17,15 @@ curriedSum = curry(sum)
 
 console.log(curriedSum(1)(2)(3))
 
+// Polyfill for map
+
+Array.prototype.customMap = function(callback,thisContext){
+    let result = []
+    for(let i=0;i<this.length;i++){
+        if(i in this){
+            result[i] = callback.call(thisContext,this[i],i,this)
+        }
+    }
+    return result
+}
+
