5e02668bde1ca471b8d0edacac3890c2b962afe0
Roman Karki <code.karki.roman@gmail.com>
2023-12-04 14:40:46 +0545

    permutation with back tracking

--- backtracking.js
+++ backtracking.js
@@ -26,3 +26,24 @@ let letterCasePermutaion = (S) => {
   dfs(0, S, []);
   return result;
 };
+
+// permutation of numbers
+let permute = (nums) => {
+  //global state
+  let result = [];
+  const dfs = (i, nums) => {
+    if (i === nums.length) {
+      result.push(nums.slice());
+      return;
+    }
+
+    // recursive dfs
+    for (j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+  dfs(0, nums);
+  return result;
+};
